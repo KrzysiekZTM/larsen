92fb1c969d186d0931c3ba5402bd7d948087f015
KrzysiekZTM <krzysiek@zostawtonam.pl>
2018-03-02 09:45:30 +0100

    zmiany...

--- contact.php
+++ contact.php
@@ -1,3 +1,13 @@
 <?php include "inc/defHeader.php" ?>
 
+<!-- 1st section -->
+
+
+<section class="full-section">
+	<img src="img/project-bg-4/jpg" alt="">
+</section>
+
+
+<!-- /1st section -->
+
 <?php include 'inc/defFooter.php' ?>
